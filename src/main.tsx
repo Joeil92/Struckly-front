cf8fcedfb5f15a2d5dba2ad7dd2b22f6097a9979
Joeil92 <93368604+Joeil92@users.noreply.github.com>
2025-05-17 00:54:54 +0200

    Shared folder added, typography component added

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './utils/i18n.ts'
+import './shared/utils/i18n.ts'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import { LoginPage } from './pages/login'
 
---
+++ src/shared/ui/Typography.tsx
@@ -0,0 +1,71 @@
+import { cva, VariantProps } from 'class-variance-authority'
+import { HTMLAttributes } from 'react'
+
+const typography = cva('', {
+    variants: {
+        tag: {
+            small: 'text-[12px] leading-[16px]',
+            span: 'text-[16px] leading-[24px]',
+            subtitle: 'text-[18px] leading-[28px] font-semibold',
+            h1: 'text-5xl leading-[58px] font-semibold',
+            h2: 'text-[40px] leading-[48px] font-semibold',
+            h3: 'text-[32px] leading-[38px] font-semibold',
+            h4: 'text-[28px] leading-[34px] font-semibold',
+            h5: 'text-[24px] leading-[28px] font-semibold',
+            p: 'text-[16px] leading-[24px]',
+        },
+    },
+})
+
+export interface TypographyProps
+    extends HTMLAttributes<HTMLElement>,
+    VariantProps<typeof typography> { }
+
+export function Typography({
+    tag = 'p',
+    className,
+    children,
+    ...props
+}: TypographyProps) {
+    return (
+        (tag === 'small' && (
+            <small className={typography({ tag, className })} {...props}>
+                {children}
+            </small>
+        )) ||
+        (tag === 'span' && (
+            <span className={typography({ tag, className })} {...props}>
+                {children}
+            </span>
+        )) ||
+        (tag === 'h1' && (
+            <h1 className={typography({ tag, className })} {...props}>
+                {children}
+            </h1>
+        )) ||
+        (tag === 'h2' && (
+            <h2 className={typography({ tag, className })} {...props}>
+                {children}
+            </h2>
+        )) ||
+        (tag === 'h3' && (
+            <h3 className={typography({ tag, className })} {...props}>
+                {children}
+            </h3>
+        )) ||
+        (tag === 'h4' && (
+            <h4 className={typography({ tag, className })} {...props}>
+                {children}
+            </h4>
+        )) ||
+        (tag === 'h5' && (
+            <h5 className={typography({ tag, className })} {...props}>
+                {children}
+            </h5>
+        )) || (
+            <p className={typography({ tag, className })} {...props}>
+                {children}
+            </p>
+        )
+    )
+}
