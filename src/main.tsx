9587f8b7ee5be0504f16b67a7d25b96ac0025dc0
Joeil92 <93368604+Joeil92@users.noreply.github.com>
2025-05-16 23:32:45 +0200

    React router added

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import './utils/i18n.ts'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import { LoginPage } from './pages/login'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
