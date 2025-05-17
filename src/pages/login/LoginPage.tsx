3c160eee8c3ccf7767bd8e5c3facc8b194907a5e
Joeil92 <93368604+Joeil92@users.noreply.github.com>
2025-05-17 15:39:12 +0200

    Button component added

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,7 +1,14 @@
+import { Button } from "../../shared/ui/Button";
+
 export function LoginPage() {
     return (
-        <div>
-            <p>Login page</p>
-        </div>
+        <>
+            <title>Struckly - Login</title>
+            <div className="p-4">
+                <Button
+                    onClick={() => console.log("login")}
+                >Se connecter</Button>
+            </div>
+        </>
     )
 }
