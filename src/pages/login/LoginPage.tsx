31ee7a62ebdba0d8c048357b9848b27e0905b2be
Joeil92 <93368604+Joeil92@users.noreply.github.com>
2025-05-18 22:59:33 +0200

    react-hook-form added, WIP login page

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,13 +1,21 @@
-import { Button } from "../../shared/ui/Button";
+import { useTranslation } from "react-i18next";
+import { Typography } from "../../shared/ui/Typography";
 
 export function LoginPage() {
+    const { t } = useTranslation();
+
     return (
         <>
-            <title>Struckly - Login</title>
-            <div className="p-4">
-                <Button
-                    onClick={() => console.log("login")}
-                >Se connecter</Button>
+            <title>{t('login.title')}</title>
+            <Typography tag={"h1"} className="fixed ps-[50px] pt-[50px]">Struckly</Typography>
+            <div className="w-full h-screen flex items-center justify-center">
+                <div className="grow flex items-center justify-center">
+                    <div className="flex flex-col gap-4">
+                        <Typography tag={"h5"}>{t('login.loginTitle')}</Typography>
+                        <Typography className="text-grey-500">{t('login.loginSubtitle')}</Typography>
+                    </div>
+                </div>
+                <div className="bg-gradient-to-b from-primary-300 via-primary-500 to-primary-600 h-screen grow"></div>
             </div>
         </>
     )
